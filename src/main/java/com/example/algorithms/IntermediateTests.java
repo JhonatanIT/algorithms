848c349654d8e6836ecba952554a6db03e89d428
JhonatanIT <jhonatan.ibatac@gmail.com>
2023-01-31 14:33:18 -0500

    feat: adding exercises

---
+++ src/main/java/com/example/algorithms/IntermediateTests.java
@@ -0,0 +1,129 @@
+package com.example.algorithms;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class IntermediateTests {
+
+    /**
+     * Given a string s of length n. The task is to find the length of the shortest substring, which upon deletion,
+     * makes the resultant string to be consisting of distinct characters only.
+     */
+    public static void shortestSubstringToDelete(String text) {
+
+        System.out.println(Math.min(findMinLength(text),
+                findMinLength(new StringBuilder(text).reverse().toString())));
+    }
+
+    public static int findMinLength(String s) {
+        int n = s.length();
+        boolean[] visited = new boolean[256];
+
+        int len = 0;
+        for (int i = 0; i < n; i++) {
+            if (visited[s.charAt(i)]) {
+                for (int j = n - 1; j >= i; j--) {
+
+                    if (visited[s.charAt(j)]) {
+                        len = j - i + 1;
+                        break;
+                    } else {
+                        visited[s.charAt(j)] = true;
+                    }
+                }
+                break;
+            } else {
+                visited[s.charAt(i)] = true;
+            }
+        }
+        return len;
+    }
+
+    /**
+     * For each word in a list of words, if any two adjacent characters are equal, change one of them. Determine
+     * the minimum number of substitutions so the final string contains no adjacent equal characters.
+     * <p>
+     * Example
+     * words = ['add', 'boook', 'break']
+     * 1. 'add': change one d (1 change)
+     * 2. 'boook': change the middle o (1 change)
+     * 3. 'break': no changes are necessary (0 changes)
+     * The return array is [1,1,0].
+     */
+    public static void noPairsAllowed(List<String> list) {
+
+        int[] ans = new int[list.size()];
+
+        for (int i = 0; i < list.size(); i++) {
+            int count = 0;
+            int j = 1;
+            String w = list.get(i);
+
+            while (j < w.length()) {
+
+                //test for match
+                if (w.charAt(j) == w.charAt(j - 1)) {
+                    //increment counter and skip the next character
+                    count += 1;
+                    j += 2;
+                } else {
+                    //move to the next character
+                    j += 1;
+                }
+            }
+
+            ans[i] = count;
+        }
+
+        System.out.println(Arrays.toString(ans));
+    }
+
+    /**
+     * Consider every subsequence of an array of integers.
+     * Sort the subsequence in increasing order.
+     * Determine the sum of differences of elements in the subsequence.
+     * Return the length of the longest subsequence where this sum is even
+     *
+     */
+    public static void findLongestSubsequence(List<Integer> list) {
+
+        list = list.stream().sorted().toList();
+
+        int firstOdd = -1;
+        int firstEven = -1;
+        int lastOdd = -1;
+        int lastEven = -1;
+
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) % 2 == 0) {
+                lastEven = i;
+                if (firstEven == -1) {
+                    firstEven = i;
+                }
+            } else {
+                lastOdd = i;
+                if (firstOdd == -1) {
+                    firstOdd = i;
+                }
+            }
+        }
+
+        int ans = 1;
+        if (firstOdd != -1) {
+            ans = Math.max(ans, lastOdd - firstOdd + 1);
+        }
+        if (firstEven != -1) {
+            ans = Math.max(ans, lastEven - firstEven + 1);
+        }
+
+        System.out.println(ans);
+    }
+
+
+    public static void main(String[] args) {
+
+//        shortestSubstringToDelete("xabbcacpqr");
+//        noPairsAllowed(List.of("ab","aab","abb","abab","abaaaba","abaaaaba"));
+        findLongestSubsequence(List.of(5, 6, 2, 3, 2, 4));
+    }
+}
